abad2122c12873722d7648d74017da82f00c534a
gdstchdr1 <72061401+gdstchdr1@users.noreply.github.com>
2023-08-02 12:21:22 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,26 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+# poweroff
+git clone https://github.com/esirplayground/luci-app-poweroff package/luci-app-poweroff
+
+#主题
+rm -rf feeds/luci/applications/luci-app-argon-config
+rm -rf feeds/kenzo/luci-app-argon-config
+git clone -b 18.06 https://github.com/jerrykuku/luci-app-argon-config.git feeds/luci/applications/luci-app-argon-config
+
+rm -rf feeds/luci/themes/luci-theme-argon
+rm -rf feeds/kenzo/luci-theme-argon
+git clone -b 18.06 https://github.com/SpeedPartner/luci-theme-argon-18.06-patch.git feeds/luci/themes/luci-theme-argon
+#git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
+
+#微信推送
+rm -rf feeds/kenzo/luci-app-serverchan
+rm -rf feeds/luci/applications/luci-app-serverchan
+git clone -b openwrt-18.06 https://github.com/tty228/luci-app-serverchan.git feeds/luci/applications/luci-app-serverchan
+
+# 修改主题背景
+cp -f $GITHUB_WORKSPACE/bg1.jpg feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
+
+./scripts/feeds update -a
+./scripts/feeds install -a
