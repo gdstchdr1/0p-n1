aac6975a0179bccdce550bbd8954e1a36c33398d
gdstchdr1 <72061401+gdstchdr1@users.noreply.github.com>
2023-08-04 12:01:35 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,7 +11,7 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.5.8/g' package/base-files/files/bin/config_generate
 # poweroff
 git clone https://github.com/esirplayground/luci-app-poweroff package/luci-app-poweroff
 
@@ -23,6 +23,7 @@ git clone -b 18.06 https://github.com/jerrykuku/luci-app-argon-config.git feeds/
 rm -rf feeds/luci/themes/luci-theme-argon
 rm -rf feeds/kenzo/luci-theme-argon
 git clone -b 18.06 https://github.com/SpeedPartner/luci-theme-argon-18.06-patch.git feeds/luci/themes/luci-theme-argon
+sed -i '41,59d' feeds/luci/themes/luci-theme-argon/luasrc/view/themes/argon/footer.htm
 #git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
 
 #微信推送
