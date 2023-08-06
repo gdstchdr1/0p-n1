a8cd3b9a3e5910eba68df02ced3dca552a7e1851
gdstchdr1 <72061401+gdstchdr1@users.noreply.github.com>
2023-08-06 12:02:17 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -26,6 +26,11 @@ git clone -b 18.06 https://github.com/SpeedPartner/luci-theme-argon-18.06-patch.
 sed -i '41,59d' feeds/luci/themes/luci-theme-argon/luasrc/view/themes/argon/footer.htm
 #git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
 
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 20.x feeds/packages/lang/golang
+
+git clone https://github.com/sbwml/luci-app-alist package/alist
+
 #微信推送
 rm -rf feeds/kenzo/luci-app-serverchan
 rm -rf feeds/luci/applications/luci-app-serverchan
