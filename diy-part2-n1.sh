c54dd0bb17457bf749d17edf05787b2c97d0d55c
gdstchdr1 <72061401+gdstchdr1@users.noreply.github.com>
2023-09-16 15:23:54 +0800

    Create diy-part2-n1.sh

---
+++ diy-part2-n1.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2-n1.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.10.8/g' package/base-files/files/bin/config_generate
+# poweroff
+# git clone https://github.com/esirplayground/luci-app-poweroff package/luci-app-poweroff
+
+#主题
+rm -rf feeds/luci/applications/luci-app-argon-config
+rm -rf feeds/kenzo/luci-app-argon-config
+git clone -b 18.06 https://github.com/jerrykuku/luci-app-argon-config.git feeds/luci/applications/luci-app-argon-config
+
+rm -rf feeds/luci/themes/luci-theme-argon
+rm -rf feeds/kenzo/luci-theme-argon
+git clone -b 18.06 https://github.com/SpeedPartner/luci-theme-argon-18.06-patch.git feeds/luci/themes/luci-theme-argon
+sed -i '41,59d' feeds/luci/themes/luci-theme-argon/luasrc/view/themes/argon/footer.htm
+#git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
+
+#修改插件源
+# rm -rf feeds/luci/applications/luci-app-aliyundrive-webdav
+
+#微信推送
+# rm -rf feeds/kenzo/luci-app-serverchan
+# rm -rf feeds/luci/applications/luci-app-serverchan
+# git clone -b openwrt-18.06 https://github.com/tty228/luci-app-serverchan.git feeds/luci/applications/luci-app-serverchan
+# rm -rf feeds/sirpdboy/luci-app-ddns-go
+# rm -rf feeds/luci/applications/luci-app-ddns-go
+git clone https://github.com/sirpdboy/luci-app-ddns-go.git package/ddns-go
+
+
+# 修改主题背景
+cp -f $GITHUB_WORKSPACE/bg1.jpg feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
+
+# 修改概览里时间显示为中文数字
+sed -i 's/os.date()/os.date("%Y年%m月%d日") .. " " .. translate(os.date("%A")) .. " " .. os.date("%X")/g' package/lean/autocore/files/arm/index.htm
+
+./scripts/feeds update -a
+./scripts/feeds install -a
